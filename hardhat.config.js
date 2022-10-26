8e9b8abcd627284797d5f73cf328fffa427857c2
Carlos Dugarte <soycarlosdugarte@gmail.com>
2022-10-25 21:22:02 -0300

    Se compila el contrato para evitar errores y se crea el script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,14 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+const { API_URL, PRIVATE_KEY } = process.env;
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  }
 };
+
